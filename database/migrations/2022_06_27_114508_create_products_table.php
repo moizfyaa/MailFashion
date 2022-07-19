de16e4f9cc871bc95e1fb9e08d79fe6c87b0d1b5
Moiz Fayaz <moizfayaz235@gmail.com>
2022-07-19 22:21:43 +0500

    Third Commit

--- database/migrations/2022_06_27_114508_create_products_table.php
+++ database/migrations/2022_06_27_114508_create_products_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            // $table->unsignedBigInteger('product_category_id');
             $table->string("name", 255)->nullable();
             $table->text("description")->nullable();
             $table->string("image", 255)->nullable();
